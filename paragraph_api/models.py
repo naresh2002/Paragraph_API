1a8426911ec2afa659015730ac9b7f6ac1543cea
naresh2002 <naresh6436@gmail.com>
2024-07-17 17:11:25 +0530

    modifying user model for tokenisation

--- paragraph_api/models.py
+++ paragraph_api/models.py
@@ -1,19 +1,45 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
-import uuid
 
-class User(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=50)
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, name, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, **extra_fields)
+        user.set_password(password) # Password Hashing
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, name, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    dob = models.DateField()
-    password = models.CharField(max_length=100)
+    dob = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
 
 
 class Paragraph(models.Model):
     text = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
 class Word(models.Model):
     word = models.CharField(max_length=255)
