e2ae75277e3b607a46913734782db64af6ebd1df
naresh2002 <naresh6436@gmail.com>
2024-07-17 16:54:21 +0530

    creating required models

--- paragraph_api/models.py
+++ paragraph_api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    dob = models.DateField()
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class Paragraph(models.Model):
+    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Word(models.Model):
+    word = models.CharField(max_length=255)
+    paragraph = models.ForeignKey(Paragraph, on_delete=models.CASCADE)
+    count = models.IntegerField()
